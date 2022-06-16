b7db946a202c3416c343947b6e9bf71a2f1d0c1e
Manuel-Antunes <nerdbroz2@gmail.com>
2022-06-16 00:14:37 -0300

    feat: install and configure flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/resources/**/*.{html,js}"],
+  content: [
+    "./src/resources/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
