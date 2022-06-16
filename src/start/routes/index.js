04ada515c034187f5b71ae6cf58e739ae834ef95
Manuel-Antunes <nerdbroz2@gmail.com>
2022-06-16 00:14:59 -0300

    feat: configure express handlebars

--- src/start/routes/index.js
+++ src/start/routes/index.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = Router();
 
 routes.get("/", (req, res) => {
-  return res.send("Hello World");
+  return res.render("index");
 });
 
 export default routes;
