5915138270a85c7ef4f03c79e3166f27c1f1c0f1
Manuel-Antunes <nerdbroz2@gmail.com>
2022-06-16 23:24:31 -0300

    fix: solve ts problems

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require("laravel-mix");
 
 mix
-  .ts("src/resources/js/app.ts", "src/public/js")
+  .ts("src/resources/ts/app.ts", "src/public/js")
   .postCss("src/resources/css/app.css", "src/public/css", [
     require("tailwindcss"),
   ]);
